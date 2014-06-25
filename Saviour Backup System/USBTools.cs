a96cb76355357c670888b4c203d50b13c94f32f0
Jake Howard <git@theorangeone.net>
2014-06-25 15:23:04 +0100

    Added Class for group of functions that control USB scanning and file checking

---
+++ Saviour Backup System/USBTools.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Saviour_Backup_System
+{
+    public class USBTools
+    {
+        public void updateDriveList()
+        {
+            //Get code for scanning drive (from other project) here
+        }
+    }
+}
