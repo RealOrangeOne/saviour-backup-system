ba28d2dc9b578f1e79eb5f5d816548b5d1398c8b
Jake Howard <git@theorangeone.net>
2014-07-21 12:39:57 +0100

    Added function to get drives

--- Saviour Backup System/USBTools.cs
+++ Saviour Backup System/USBTools.cs
@@ -9,9 +9,20 @@ namespace Saviour_Backup_System
 {
     public class USBTools
     {
-        public void updateDriveList()
+        public DriveInfo[] getConnectedDrives()
         {
-            //Get code for scanning drive (from other project) here
+            List<DriveInfo> drivesList = new List<DriveInfo>();
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives) {
+                try {
+                    string driveName = drive.VolumeLabel;
+                    string driveLetter = drive.Name;
+                } catch { // If there is a problem getting the drive data, then the program would crash!
+                    continue;
+                }
+                drivesList.Add(drive);
+            }
+            return drivesList.ToArray(); 
         }
     }
 }
