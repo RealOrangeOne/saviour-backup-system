23b2fe177bc6a86733812dfa07d489fd165ad3bc
Jake Howard <git@theorangeone.net>
2014-07-22 23:24:20 +0100

    Began development on function for scanning inserted drives

--- Saviour Backup System/USBTools.cs
+++ Saviour Backup System/USBTools.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Timers;
 
 namespace Saviour_Backup_System
 {
     public class USBTools
     {
+        public static void initDriveScan(){
+            Timer scanTimer = new Timer();
+            scanTimer.Elapsed += new ElapsedEventHandler(driveScanTick);
+            scanTimer.Interval = 5000;
+            scanTimer.Start();
+        }
+        private static void driveScanTick(object sender, ElapsedEventArgs e)
+        {
+
+        }
         public static DriveInfo[] getConnectedDrives()
         {
             List<DriveInfo> drivesList = new List<DriveInfo>();
