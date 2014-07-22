c8930295965ce784527f516841a660e7edced7cb
Jake Howard <git@theorangeone.net>
2014-07-22 21:14:30 +0100

    Added refresh code for button, and began development for selecting drive

--- Saviour Backup System/mainWindow.cs
+++ Saviour Backup System/mainWindow.cs
@@ -16,10 +16,32 @@ namespace Saviour_Backup_System
         public mainWindow()
         {
             InitializeComponent();
-            ListViewItem item1 = new ListViewItem("Something");
-            item1.SubItems.Add("SubItem1a");
-            item1.SubItems.Add("SubItem1b");
-            listViewEx1.Items.AddRange(new ListViewItem[] { item1});
+            refreshDriveList();
         }
+        public void refreshDriveList()
+        {
+            DriveInfo[] drives = USBTools.getConnectedDrives();
+            connectedDrivesList.Items.Clear();
+            foreach (DriveInfo drive in drives){
+                ListViewItem driveItem = new ListViewItem(drive.Name + " " + drive.VolumeLabel);
+                driveItem.SubItems.Add("X");
+                connectedDrivesList.Items.Add(driveItem);
+            }
+            connectedDrivesList.Sort();
+        }
+
+        private void connectedDrivesListRefresh_Click(object sender, EventArgs e)
+        {
+            refreshDriveList();
+        }
+
+        private void connectedDrivesList_Click(object sender, EventArgs e)
+        {
+            string currentItem = connectedDrivesList.SelectedItems.ToString();
+
+            int index = connectedDrivesList.FindString(currentItem);
+
+        }
+
     }
 }
