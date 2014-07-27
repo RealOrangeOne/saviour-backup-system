637baf6e6491bedb3d4e6ec93c4b38a3034425f2
Jake Howard <git@theorangeone.net>
2014-07-27 20:55:59 +0100

    added tools file to contain global useful functions

---
+++ Saviour Backup System/tools.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saviour_Backup_System
+{
+    class tools
+    {
+        public static string Trim(this string value, int maxLength)
+        {
+            if (value.Length > maxLength)
+            {
+                return "..." + value.Substring(value.Length - (maxLength - 3));
+            }
+
+            return value;
+        }
+    }
+}
