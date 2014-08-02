ef38c3678cd8c7766233d7cf6df9375e07606cca
Jake Howard <git@theorangeone.net>
2014-08-02 10:50:46 +0100

    added icons and files

---
+++ Saviour Backup System/notificationIcon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Saviour_Backup_System
+{
+    class notificationIcon
+    {
+        internal static NotifyIcon notifyIcon;
+        internal static ContextMenu contextMenu;
+
+        internal static void init()
+        {
+            contextMenu = new ContextMenu();
+            contextMenu.MenuItems.Add("Show Interface", displayWindow);
+
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Text = "Saviour Backup System";
+            notifyIcon.Icon = Properties.Resources.redCDIconICO;
+
+            notifyIcon.ContextMenu = contextMenu;
+            notifyIcon.Visible = true; //finally displays the tray icon
+        }
+
+        private static void displayWindow(object sender, EventArgs e)
+        {
+            MessageBox.Show("Display");
+        }
+    }
+}
