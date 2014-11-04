c84e2381e3278ba355a329af0fd578cf9dfcaae9
Jake Howard <git@theorangeone.net>
2014-11-04 17:26:59 +0000

    moved context menu code and made setup function, also added 'still running' balloontip

--- Saviour Backup System/notificationIcon.cs
+++ Saviour Backup System/notificationIcon.cs
@@ -10,19 +10,29 @@ namespace Saviour_Backup_System
     class notificationIcon
     {
         internal NotifyIcon notifyIcon = new NotifyIcon() ;
-        internal static ContextMenu contextMenu = new ContextMenu();
+        internal ContextMenu contextMenu = new ContextMenu();
 
         internal notificationIcon()
         {
-            contextMenu.MenuItems.Add("Show Interface", displayWindow);
-
+           
             notifyIcon.Text = "Saviour Backup System";
             notifyIcon.Icon = Properties.Resources.redCDIconICO;
-
+            populateList();
             notifyIcon.ContextMenu = contextMenu;
             notifyIcon.Visible = true; //finally displays the tray icon
         }
+        private void populateList()
+        {
+            contextMenu.MenuItems.Add("Show Interface", displayWindow);
 
-        private static void displayWindow(object sender, EventArgs e) { setup.MW.Show(); }
+        }
+        private void displayWindow(object sender, EventArgs e) { setup.MW.showDisplay(); }
+
+        internal void displayStillRunning() {
+            notifyIcon.BalloonTipText = "Saviour backup system is still running in the background";
+            notifyIcon.BalloonTipTitle = "Still running...";
+            notifyIcon.ShowBalloonTip(2000);
+
+        }
     }
 }
