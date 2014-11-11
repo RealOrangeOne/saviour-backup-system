aed7839c40ea7e5b336b5c40cdea965c3c5fe394
Jake Howard <git@theorangeone.net>
2014-11-11 22:49:16 +0000

    added label to splash screen and changed / confirmed properties

--- Saviour Backup System/splashScreen.Designer.cs
+++ Saviour Backup System/splashScreen.Designer.cs
@@ -29,7 +29,20 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(splashScreen));
+            this.desc = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // desc
+            // 
+            this.desc.AutoSize = true;
+            this.desc.BackColor = System.Drawing.Color.Transparent;
+            this.desc.Font = new System.Drawing.Font("Lucida Sans Typewriter", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.desc.ForeColor = System.Drawing.Color.White;
+            this.desc.Location = new System.Drawing.Point(81, 176);
+            this.desc.Name = "desc";
+            this.desc.Size = new System.Drawing.Size(88, 16);
+            this.desc.TabIndex = 0;
+            this.desc.Text = "The Label!";
             // 
             // splashScreen
             // 
@@ -39,17 +52,22 @@
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(408, 198);
             this.ControlBox = false;
+            this.Controls.Add(this.desc);
             this.Cursor = System.Windows.Forms.Cursors.AppStarting;
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "splashScreen";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Savior Backup System";
             this.TransparencyKey = System.Drawing.Color.White;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label desc;
     }
 }
