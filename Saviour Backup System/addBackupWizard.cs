76965362217ff0a7292dc2b2c01157914ee04b46
Jake Howard <git@theorangeone.net>
2014-11-15 13:44:37 +0000

    added code for listing drives and getting directory

--- Saviour Backup System/addBackupWizard.cs
+++ Saviour Backup System/addBackupWizard.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Saviour_Backup_System
 {
     public partial class addBackupWizard : Form
     {
+        private string folderPath = "";
         public addBackupWizard()
         {
             InitializeComponent();
+            populateDropdown();            
+        }
+
+        private void directoryBrowseButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = folderBrowserDialog.SelectedPath;
+            }
+            folderBrowserDialog.Dispose(); //memory management
+        }
+
+        private void populateDropdown()
+        {
+            DriveInfo[] drives = USBTools.getConnectedDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                drivesDropdown.Items.Add(drive.VolumeLabel);
+            }
         }
     }
 }
