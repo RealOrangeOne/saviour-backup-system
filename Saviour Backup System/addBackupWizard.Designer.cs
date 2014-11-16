30231e6becc099d2a40d968b215fc6189ebbbbd9
Jake Howard <git@theorangeone.net>
2014-11-16 15:49:48 +0000

    adjusted label position

--- Saviour Backup System/addBackupWizard.Designer.cs
+++ Saviour Backup System/addBackupWizard.Designer.cs
@@ -95,7 +95,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Lucida Sans Unicode", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(331, 114);
+            this.label3.Location = new System.Drawing.Point(350, 114);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(76, 18);
             this.label3.TabIndex = 7;
