2c362daa0374df46f6975882482894e549ee82b2
Jake Howard <git@theorangeone.net>
2014-11-17 16:11:43 +0000

    modify elements

--- Saviour Backup System/addBackupWizard.Designer.cs
+++ Saviour Backup System/addBackupWizard.Designer.cs
@@ -32,7 +32,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.drivesDropdown = new System.Windows.Forms.ComboBox();
-            this.title = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.folderPath = new System.Windows.Forms.TextBox();
             this.directoryBrowseButton = new System.Windows.Forms.Button();
@@ -44,14 +43,18 @@
             this.label9 = new System.Windows.Forms.Label();
             this.compressionTypeDropdown = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.integerInput1 = new DevComponents.Editors.IntegerInput();
-            ((System.ComponentModel.ISupportInitialize)(this.integerInput1)).BeginInit();
+            this.previousBackupInput = new DevComponents.Editors.IntegerInput();
+            this.createButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.previousBackupInput)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox2
             // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox2.Font = new System.Drawing.Font("Lucida Sans Unicode", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(302, 42);
+            this.textBox2.Location = new System.Drawing.Point(302, 9);
             this.textBox2.MaxLength = 30;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(249, 28);
@@ -61,7 +64,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(183, 43);
+            this.label1.Location = new System.Drawing.Point(183, 10);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(113, 18);
             this.label1.TabIndex = 2;
@@ -71,7 +74,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Lucida Sans Unicode", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(245, 77);
+            this.label2.Location = new System.Drawing.Point(245, 44);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(51, 18);
             this.label2.TabIndex = 4;
@@ -79,30 +82,24 @@
             // 
             // drivesDropdown
             // 
+            this.drivesDropdown.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.drivesDropdown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.drivesDropdown.Font = new System.Drawing.Font("Lucida Sans Unicode", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.drivesDropdown.FormattingEnabled = true;
-            this.drivesDropdown.Location = new System.Drawing.Point(302, 76);
+            this.drivesDropdown.Location = new System.Drawing.Point(302, 43);
             this.drivesDropdown.MaxDropDownItems = 26;
             this.drivesDropdown.Name = "drivesDropdown";
             this.drivesDropdown.Size = new System.Drawing.Size(249, 24);
             this.drivesDropdown.Sorted = true;
             this.drivesDropdown.TabIndex = 5;
             // 
-            // title
-            // 
-            this.title.AutoSize = true;
-            this.title.Font = new System.Drawing.Font("Lucida Sans Unicode", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.title.Location = new System.Drawing.Point(12, 9);
-            this.title.Name = "title";
-            this.title.Size = new System.Drawing.Size(162, 28);
-            this.title.TabIndex = 8;
-            this.title.Text = "Add a Backup";
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Lucida Sans Unicode", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(12, 243);
+            this.label5.Location = new System.Drawing.Point(12, 195);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(81, 18);
             this.label5.TabIndex = 10;
@@ -110,8 +107,11 @@
             // 
             // folderPath
             // 
+            this.folderPath.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.folderPath.Font = new System.Drawing.Font("Lucida Sans Unicode", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.folderPath.Location = new System.Drawing.Point(99, 239);
+            this.folderPath.Location = new System.Drawing.Point(99, 191);
             this.folderPath.MaxLength = 30;
             this.folderPath.Name = "folderPath";
             this.folderPath.Size = new System.Drawing.Size(376, 28);
@@ -120,7 +120,7 @@
             // directoryBrowseButton
             // 
             this.directoryBrowseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.directoryBrowseButton.Location = new System.Drawing.Point(476, 239);
+            this.directoryBrowseButton.Location = new System.Drawing.Point(476, 191);
             this.directoryBrowseButton.Name = "directoryBrowseButton";
             this.directoryBrowseButton.Size = new System.Drawing.Size(75, 28);
             this.directoryBrowseButton.TabIndex = 11;
@@ -132,7 +132,7 @@
             // 
             this.introTextBox.BackColor = System.Drawing.SystemColors.Control;
             this.introTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.introTextBox.Location = new System.Drawing.Point(2, 45);
+            this.introTextBox.Location = new System.Drawing.Point(2, 12);
             this.introTextBox.Name = "introTextBox";
             this.introTextBox.ReadOnly = true;
             this.introTextBox.Size = new System.Drawing.Size(175, 176);
@@ -151,7 +151,7 @@
             this.switchButton1.BackgroundStyle.Class = "";
             this.switchButton1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
             this.switchButton1.Font = new System.Drawing.Font("Lucida Sans Typewriter", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.switchButton1.Location = new System.Drawing.Point(487, 181);
+            this.switchButton1.Location = new System.Drawing.Point(487, 133);
             this.switchButton1.Name = "switchButton1";
             this.switchButton1.OffBackColor = System.Drawing.Color.DarkRed;
             this.switchButton1.OffTextColor = System.Drawing.Color.White;
@@ -170,7 +170,7 @@
             this.switchButton3.BackgroundStyle.Class = "";
             this.switchButton3.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
             this.switchButton3.Font = new System.Drawing.Font("Lucida Sans Typewriter", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.switchButton3.Location = new System.Drawing.Point(487, 210);
+            this.switchButton3.Location = new System.Drawing.Point(487, 162);
             this.switchButton3.Name = "switchButton3";
             this.switchButton3.OffBackColor = System.Drawing.Color.DarkRed;
             this.switchButton3.OffTextColor = System.Drawing.Color.White;
@@ -184,7 +184,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(343, 184);
+            this.label6.Location = new System.Drawing.Point(343, 136);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(139, 18);
             this.label6.TabIndex = 16;
@@ -194,7 +194,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(347, 212);
+            this.label8.Location = new System.Drawing.Point(347, 164);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(134, 18);
             this.label8.TabIndex = 18;
@@ -204,7 +204,7 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Lucida Sans Unicode", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(186, 107);
+            this.label9.Location = new System.Drawing.Point(186, 74);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(110, 18);
             this.label9.TabIndex = 20;
@@ -212,12 +212,16 @@
             // 
             // compressionTypeDropdown
             // 
+            this.compressionTypeDropdown.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.compressionTypeDropdown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.compressionTypeDropdown.Font = new System.Drawing.Font("Lucida Sans Unicode", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.compressionTypeDropdown.FormattingEnabled = true;
             this.compressionTypeDropdown.Items.AddRange(new object[] {
             "None",
             "ZIP Archive (*.zip)"});
-            this.compressionTypeDropdown.Location = new System.Drawing.Point(302, 106);
+            this.compressionTypeDropdown.Location = new System.Drawing.Point(302, 73);
             this.compressionTypeDropdown.MaxDropDownItems = 26;
             this.compressionTypeDropdown.Name = "compressionTypeDropdown";
             this.compressionTypeDropdown.Size = new System.Drawing.Size(249, 24);
@@ -228,34 +232,49 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Lucida Sans Unicode", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(325, 139);
+            this.label3.Location = new System.Drawing.Point(325, 106);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(140, 18);
             this.label3.TabIndex = 23;
             this.label3.Text = "Previous Backups:";
             // 
-            // integerInput1
+            // previousBackupInput
+            // 
+            this.previousBackupInput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             // 
             // 
             // 
+            this.previousBackupInput.BackgroundStyle.Class = "DateTimeInputBackground";
+            this.previousBackupInput.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.previousBackupInput.ButtonFreeText.Shortcut = DevComponents.DotNetBar.eShortcut.F2;
+            this.previousBackupInput.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.previousBackupInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.previousBackupInput.Location = new System.Drawing.Point(471, 103);
+            this.previousBackupInput.MinValue = -1;
+            this.previousBackupInput.Name = "previousBackupInput";
+            this.previousBackupInput.ShowUpDown = true;
+            this.previousBackupInput.Size = new System.Drawing.Size(80, 24);
+            this.previousBackupInput.TabIndex = 25;
             // 
-            this.integerInput1.BackgroundStyle.Class = "DateTimeInputBackground";
-            this.integerInput1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-            this.integerInput1.ButtonFreeText.Shortcut = DevComponents.DotNetBar.eShortcut.F2;
-            this.integerInput1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.integerInput1.Location = new System.Drawing.Point(471, 136);
-            this.integerInput1.MinValue = -1;
-            this.integerInput1.Name = "integerInput1";
-            this.integerInput1.ShowUpDown = true;
-            this.integerInput1.Size = new System.Drawing.Size(80, 24);
-            this.integerInput1.TabIndex = 25;
+            // createButton
+            // 
+            this.createButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createButton.Location = new System.Drawing.Point(395, 225);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(156, 27);
+            this.createButton.TabIndex = 27;
+            this.createButton.Text = "Create";
+            this.createButton.UseVisualStyleBackColor = true;
             // 
             // addBackupWizard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(563, 436);
-            this.Controls.Add(this.integerInput1);
+            this.ClientSize = new System.Drawing.Size(563, 257);
+            this.Controls.Add(this.createButton);
+            this.Controls.Add(this.previousBackupInput);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.compressionTypeDropdown);
             this.Controls.Add(this.label9);
@@ -267,14 +286,19 @@
             this.Controls.Add(this.directoryBrowseButton);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.folderPath);
-            this.Controls.Add(this.title);
             this.Controls.Add(this.drivesDropdown);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "addBackupWizard";
-            this.Text = "addBackupWizard";
-            ((System.ComponentModel.ISupportInitialize)(this.integerInput1)).EndInit();
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Add Backup Wizard";
+            ((System.ComponentModel.ISupportInitialize)(this.previousBackupInput)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -286,7 +310,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox drivesDropdown;
-        private System.Windows.Forms.Label title;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox folderPath;
         private System.Windows.Forms.Button directoryBrowseButton;
@@ -298,6 +321,7 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.ComboBox compressionTypeDropdown;
         private System.Windows.Forms.Label label3;
-        private DevComponents.Editors.IntegerInput integerInput1;
+        private DevComponents.Editors.IntegerInput previousBackupInput;
+        private System.Windows.Forms.Button createButton;
     }
 }
