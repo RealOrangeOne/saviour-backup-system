1ec1b2dae119b26a3c97ef2513b17c88b9e0fad5
Jake Howard <git@theorangeone.net>
2014-11-21 22:28:16 +0000

    added basic elements to UI

--- Saviour Backup System/currentTransfers.Designer.cs
+++ Saviour Backup System/currentTransfers.Designer.cs
@@ -31,6 +31,8 @@
             this.components = new System.ComponentModel.Container();
             this.ribbonControl1 = new DevComponents.DotNetBar.RibbonControl();
             this.styleManager1 = new DevComponents.DotNetBar.StyleManager(this.components);
+            this.progressTemplate = new DevComponents.DotNetBar.Controls.ProgressBarX();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ribbonControl1
@@ -47,7 +49,7 @@
             this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
             this.ribbonControl1.Name = "ribbonControl1";
             this.ribbonControl1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 2);
-            this.ribbonControl1.Size = new System.Drawing.Size(566, 31);
+            this.ribbonControl1.Size = new System.Drawing.Size(592, 31);
             this.ribbonControl1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
             this.ribbonControl1.SystemText.MaximizeRibbonText = "&Maximize the Ribbon";
             this.ribbonControl1.SystemText.MinimizeRibbonText = "Mi&nimize the Ribbon";
@@ -74,16 +76,46 @@
             this.styleManager1.ManagerStyle = DevComponents.DotNetBar.eStyle.Office2010Black;
             this.styleManager1.MetroColorParameters = new DevComponents.DotNetBar.Metro.ColorTables.MetroColorGeneratorParameters(System.Drawing.Color.White, System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(163)))), ((int)(((byte)(26))))));
             // 
+            // progressTemplate
+            // 
+            // 
+            // 
+            // 
+            this.progressTemplate.BackgroundStyle.Class = "";
+            this.progressTemplate.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.progressTemplate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.progressTemplate.Location = new System.Drawing.Point(12, 59);
+            this.progressTemplate.Name = "progressTemplate";
+            this.progressTemplate.Size = new System.Drawing.Size(568, 26);
+            this.progressTemplate.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.progressTemplate.TabIndex = 1;
+            this.progressTemplate.Text = "Backup in Progress";
+            this.progressTemplate.TextVisible = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
             // currentTransfers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(566, 261);
+            this.ClientSize = new System.Drawing.Size(592, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressTemplate);
             this.Controls.Add(this.ribbonControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "currentTransfers";
             this.Text = "Current Transfers";
+            this.Load += new System.EventHandler(this.currentTransfers_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -91,6 +123,8 @@
 
         private DevComponents.DotNetBar.RibbonControl ribbonControl1;
         private DevComponents.DotNetBar.StyleManager styleManager1;
+        private DevComponents.DotNetBar.Controls.ProgressBarX progressTemplate;
+        private System.Windows.Forms.Label label1;
 
     }
 }
