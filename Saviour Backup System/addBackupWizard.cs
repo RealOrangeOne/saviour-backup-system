812d63858be576af5b07acbf08c49bcb8e046e61
Jake Howard <git@theorangeone.net>
2014-11-25 22:20:37 +0000

    added more verification and prepared for database addition

--- Saviour Backup System/addBackupWizard.cs
+++ Saviour Backup System/addBackupWizard.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading;
 using System.Windows.Forms;
 using System.IO;
+using System.Data.SqlServerCe;
 
 namespace Saviour_Backup_System
 {
@@ -41,8 +42,12 @@ namespace Saviour_Backup_System
             }
         }
 
-        private void createButton_Click(object sender, EventArgs e)
-        {
+        private void createButton_Click(object sender, EventArgs e) {
+            if ((folderPath.Text == "") || (previousBackupInput.Text == "") || (compressionTypeDropdown.Text == "") || (drivesDropdown.Text == "")){
+                    MessageBox.Show("You have not filled in every element, Please try again!", "Not everything is complete", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+            }
+
             statusProgress.Text = "Initialising...";
             int initHeight = 299;
             while (this.Size.Height != 330) {
@@ -50,36 +55,41 @@ namespace Saviour_Backup_System
                 this.Size = new Size(583, initHeight);
                 Thread.Sleep(10);
             }
-            try { //check validity of input from user
 
-                if ((folderPath.Text == "") || (previousBackupInput.Text == "") || (compressionTypeDropdown.Text == "") || (drivesDropdown.Text == "")){
-                    MessageBox.Show("You have not filled in every element, Please try again!", "Not everything is complete", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-
-
-                if (!Directory.Exists(folderPath.Text) {
-                    DialogResult result = MessageBox.Show("The folder path you have entered doesnt exist, would you like to create it?", "Create Folder", MessageBoxButtons.YesNo);
-                    if (result == DialogResult.Yes){
-                        try{
-                            Directory.CreateDirectory(folderPath.Text);
-                        } catch{
-                            MessageBox.Show("Error Creating Folder!");
-                            return;
-                        }
-                    } else {
-                        return;
+            statusProgress.Text = "Checking form...";     
+            if (!Directory.Exists(folderPath.Text)) {
+                DialogResult result = MessageBox.Show("The folder path you have entered doesnt exist, would you like to create it?", "Create Folder", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes) {
+                    try { Directory.CreateDirectory(folderPath.Text);
+                    } catch {
+                        MessageBox.Show("Error Creating Folder! Please check the path and try agian.", "Error creating folder", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
                 }
-            } finally {
-                while (this.Size.Height != 299) {
-                    initHeight--;
-                    this.Size = new Size(583, initHeight);
-                    Thread.Sleep(10);
-                }
-                statusProgress.ResetText();
-                statusProgress.Value = 0;
             }
+
+            else if (compressionTypeDropdown.Text == "None" && unifiedFileSwitch.Value == true) {
+                MessageBox.Show("You cannot have a unified file without some form of compression, please select again.", "Compression Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                createRecord();
+            }
+            while (this.Size.Height != 299) {
+                initHeight--;
+                this.Size = new Size(583, initHeight);
+                Thread.Sleep(10);
+            }
+        }
+
+        private void createRecord()
+        {
+            SqlCeConnection conn = databaseTools.conn;
+            SqlCeCommand cmd = conn.CreateCommand();
+            conn.Open();
+
+            conn.Close();
+            MessageBox.Show("Record created successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
         }
     }
 }
