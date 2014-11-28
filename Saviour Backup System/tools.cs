2d61ae8f62d02d30fe051318a404412b452fba36
Jake Howard <git@theorangeone.net>
2014-11-28 17:14:52 +0000

    added function for calculating hash (MD5 format)

--- Saviour Backup System/tools.cs
+++ Saviour Backup System/tools.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace Saviour_Backup_System
 {
@@ -15,6 +16,22 @@ namespace Saviour_Backup_System
                 return value.Substring(0, maxLength - 3) + "...";
             }
             return value;
+        }
+
+        public static string hash(string input)
+        {
+            // step 1, calculate MD5 hash from input
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // step 2, convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
         }
     }
 }
