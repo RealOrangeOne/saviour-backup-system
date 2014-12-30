1b6ba426395e665acb8b13df94fe1bca5f37e4b4
Jake Howard <git@theorangeone.net>
2014-12-30 17:33:51 +0000

    added code to reverse timestamp operation

--- Saviour Backup System/tools.cs
+++ Saviour Backup System/tools.cs
@@ -62,5 +62,13 @@ namespace Saviour_Backup_System
 
             return BitConverter.ToString(md5.Hash).Replace("-", "").ToLower();
         }
+
+        public static DateTime unixDateTime(long unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
     }
 }
