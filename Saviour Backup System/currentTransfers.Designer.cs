09d7072181ce6a011b6d187ad542c1070646cfcf
Jake Howard <git@theorangeone.net>
2015-01-01 14:23:23 +0000

    removed testing button

--- Saviour Backup System/currentTransfers.Designer.cs
+++ Saviour Backup System/currentTransfers.Designer.cs
@@ -33,7 +33,6 @@
             this.styleManager1 = new DevComponents.DotNetBar.StyleManager(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.layoutPanel = new System.Windows.Forms.FlowLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
             this.ClearButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -96,16 +95,6 @@
             this.layoutPanel.Size = new System.Drawing.Size(592, 391);
             this.layoutPanel.TabIndex = 3;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(367, 33);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // ClearButton
             // 
             this.ClearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -123,7 +112,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(592, 450);
             this.Controls.Add(this.ClearButton);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.layoutPanel);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.ribbonControl1);
@@ -141,7 +129,6 @@
         private DevComponents.DotNetBar.RibbonControl ribbonControl1;
         private DevComponents.DotNetBar.StyleManager styleManager1;
         private System.Windows.Forms.FlowLayoutPanel layoutPanel;
-        private System.Windows.Forms.Button button1;
         public System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button ClearButton;
 
