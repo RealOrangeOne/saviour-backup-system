90ea4ec576998f0b037caf8b509639e420cd824c
Jake Howard <git@theorangeone.net>
2015-01-12 11:22:00 +0000

    updated form, so multiselect doesnt work, and added update code (untested)

--- Saviour Backup System/backupViewer.Designer.cs
+++ Saviour Backup System/backupViewer.Designer.cs
@@ -108,8 +108,10 @@
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
             this.dataGridView.Location = new System.Drawing.Point(0, 82);
+            this.dataGridView.MultiSelect = false;
             this.dataGridView.Name = "dataGridView";
             this.dataGridView.ReadOnly = true;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView.ShowEditingIcon = false;
             this.dataGridView.Size = new System.Drawing.Size(544, 196);
             this.dataGridView.TabIndex = 1;
@@ -154,6 +156,7 @@
             this.editButton.TabIndex = 7;
             this.editButton.Text = "button3";
             this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
             // 
             // button4
             // 
